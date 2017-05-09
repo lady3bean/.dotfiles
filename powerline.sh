370c6dc169370b153d0b736e2085612519393f34
Hawley <hawley.brett@moveon.org>
2017-05-09 16:55:17 -0400

    Simple tests

---
+++ powerline.sh
@@ -0,0 +1,6 @@
+powerline-daemon -q
+export POWERLINE_COMMAND=powerline
+export POWERLINE_CONFIG_COMMAND=powerline-config
+POWERLINE_BASH_CONTINUATION=1
+POWERLINE_BASH_SELECT=1
+. $HOME/Library/Python/2.7/lib/python/site-packages/powerline/bindings/bash/powerline.sh
